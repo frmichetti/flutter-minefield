b8d851765381d2e7c02921d1d2b0f0bcd5aaab25
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-01-19 16:33:46 -0300

    force portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/minefield_app.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MineFieldApp());
 }
